39e732e2d2e8d9ec8b99ccb42a3d8334df0fcaf0
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2024-06-12 16:56:24 +0930

    REL: Update version number

--- phables/workflow/scripts/phables_utils/output_utils.py
+++ phables/workflow/scripts/phables_utils/output_utils.py
@@ -5,7 +5,7 @@ import subprocess
 FASTA_LINE_LEN = 60
 
 # Create logger
-logger = logging.getLogger("phables 1.3.3")
+logger = logging.getLogger("phables 1.4.0")
 
 
 def write_unitigs(nodes, unitig_names, graph_unitigs, filename, output):
